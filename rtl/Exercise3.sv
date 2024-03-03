c0318a8fcff5f0ad3ab3cea043d5e4238d7fe44a
James Jin <jamesjin0516@gmail.com>
2024-03-02 23:44:05 -0500

    exericse(3): removed include statements and passed tests.

--- rtl/Exercise3.sv
+++ rtl/Exercise3.sv
@@ -1,6 +1,3 @@
-//`include "rtl/Mysteries/Mystery1.sv"
-//`include "rtl/Mysteries/Mystery2.sv"
-
 module Exercise3 (
     input clk,
     input nReset,
@@ -12,10 +9,10 @@ module Exercise3 (
 
 wire [7:0] md1_out;
 wire [7:0] md2_out;
-/*
+
 Mystery1 md1_1 (a[1:0], b[7:0], c[7:0], md1_out);
 Mystery1 md1_2 (a[3:2], b[15:8], c[15:8], md2_out);
 
 Mystery2 md2 (clk, nReset, md1_out, md2_out, out);
-*/
+
 endmodule
